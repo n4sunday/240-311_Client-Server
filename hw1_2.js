32758b1b5749ad473fc97cc27a0a0e4bd4907fd5
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-01-31 03:09:46 +0700

    update hw1_2

--- hw1_2.js
+++ hw1_2.js
@@ -1,10 +1,12 @@
 var response = require('./cal.js')
+let stdin = process.openStdin()
 console.log("Enter Number x:");
-process.stdin.on('data', function (x) {
+stdin.addListener('data', (x) => {
     var y = x.toString()
     for (var i = 1; i <= 12; i++) {
         result = response.mul(x, i)
         console.log("x*" + i + "=" + result)
     }
+    stdin.destroy()
 });
 
