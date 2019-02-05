0cbd1825ad073d529d9ecc56118b326750467ab9
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-02-06 02:53:13 +0700

    Lab 2

--- tcp_client.js
+++ tcp_client.js
@@ -1,16 +1,43 @@
 var net = require('net');
 var HOST = '127.0.0.1';
-var PORT = 8500;
+var PORT = 6969;
+var NUMBER = 0;
+var COUNT = 0;
 var client = new net.Socket();
-client.connect(PORT, HOST, function () {
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
-    client.write('I am Chuck Norris!');
+client.connect(PORT, HOST, function() {
+   console.log('CONNECTED TO: ' + HOST + ':' + PORT);
+   client.write('5935512004');
 });
-client.on('data', function (data) {
-    console.log('DATA: ' + data);
-    client.destroy();
+
+client.on('data', function(data) {
+    if(COUNT>=5){
+        client.destroy();
+    }
+    else if(data=='OK'){
+        console.log('OK')
+        console.log('Client -----> '+NUMBER)
+        client.write(''+NUMBER);
+        NUMBER++
+        COUNT++
+    }
+    else if(data=='WRONG'){
+        console.log('WRONG')
+        client.write(''+NUMBER);
+        console.log('Client -----> '+NUMBER)
+        NUMBER++
+        COUNT++
+    }
+    else if(data=='BINGO'){
+        console.log('BINGO')
+        console.log('Client -----> '+NUMBER)
+        console.log(''+COUNT)
+        client.destroy();
+    }
+   //console.log('DATA: ' + data);
+  // client.destroy();
 });
+
 // Add a 'close' event handler for the client socket
-client.on('close', function () {
-    console.log('Connection closed');
+client.on('close', function() {
+   console.log('Connection closed');
 });
--- tcp_server.js
+++ tcp_server.js
@@ -1,14 +1,42 @@
 var net = require('net');
+
 var HOST = '127.0.0.1';
-var PORT = 8500;
-net.createServer(function (sock) {
-    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
-    sock.on('data', function (data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        sock.write('You said "' + data + '"');
-    });
-    sock.on('close', function (data) {
-        console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
-    });
+var PORT = 6969;
+let answer=Math.floor(Math.random() * 21)
+net.createServer(function(sock) {
+    console.log('Answer : '+answer);
+    
+   console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+   var COUNT = 5;
+   sock.on('data', function(data) {
+       //console.log('DATA ' + sock.remoteAddress);
+       if(COUNT==0){
+           sock.destroy()
+       }
+       //sock.write('OK "' + data + '"');
+       else if(data=='5935512004'){
+            sock.write('OK')
+            console.log('Client -----> '+data)
+            console.log('OK <----- Server')
+            
+       }
+       else if(data==''+answer){
+            sock.write('BINGO')
+            console.log('Client -----> '+data)
+            console.log('BINGO <----- Server')
+            sock.destroy()
+            
+       }
+       else if(COUNT>=0){
+        
+            sock.write('WRONG')
+            console.log('Client -----> '+data)
+            console.log('WRONG <----- Server')
+            COUNT-- 
+       }
+   });
+
+   
 }).listen(PORT, HOST);
-console.log('Server listening on ' + HOST + ':' + PORT);
+
+console.log('Server listening on ' + HOST +':'+ PORT);
